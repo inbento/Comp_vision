fa17468aba0e2e5f7988b2ec02a0b00a5d775299
inbento <mikov.ns.04@gmail.com>
2025-10-03 22:22:59 +0300

    Task3-5. Сравнили СВОЮ фильтрацию и встроенную

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -12,9 +12,8 @@ def gaussian_function(size, sigma):
     for i in range(size):
         for j in range(size):
             x, y  = i, j
-
-            exp = math.exp(-1 * ((x - a) ** 2 + (y - b) ** 2) / 2 * sigma ** 2)
-            gauss = exp / 2 * math.pi * sigma ** 2
+            exp = math.exp(-((x - a)**2 + (y - b)**2) / (2 * sigma**2))
+            gauss = exp / 2 * math.pi * sigma**2
             kernel[i,j] = gauss
             elements_summ += gauss
 
@@ -23,8 +22,31 @@ def gaussian_function(size, sigma):
     return kernel
 
 
-sigma_values = [0.66, 0.33]
-matrix_sizes = [5, 7]
+def super_svertka(img, kernel_size, sigma):
+    kernel = gaussian_function(kernel_size, sigma)
+
+    padding_size = kernel_size // 2
+    img_padded = cv2.copyMakeBorder(img, padding_size, padding_size, padding_size, padding_size, cv2.BORDER_REFLECT)
+
+    gauss_blur = img.copy()
+    height, width = img.shape
+    
+    for y in range(height):
+        for x in range(width):
+            value = 0
+            y_padded = y + padding_size
+            x_padded = x + padding_size
+            
+            for k in range(kernel_size):
+                for l in range(kernel_size):
+                    value += img_padded[y_padded + k - padding_size, x_padded + l - padding_size] * kernel[k, l]
+            
+            gauss_blur[y, x] = value
+    
+    return gauss_blur
+
+sigma_values = [3, 2]
+matrix_sizes = [9, 11]
 
 img = cv2.imread(r"E:/GitHub/Comp_vision/lab3/img2.jfif", cv2.IMREAD_GRAYSCALE)
 
@@ -33,17 +55,13 @@ cv2.imshow('Original image', img)
 
 for size in matrix_sizes:
     for sigma in sigma_values:
-        
-        gauss_matrix = gaussian_function(size, sigma)
 
-        print(f"\n" + "-" * 30)
-        print(f"Нормированная матрица Гаусса для размера: {size} и среднеквадратичного отклонения: {sigma}")
-        print(gauss_matrix)
-
-        filtered_img = cv2.filter2D(img, -1, gauss_matrix)
-
+        #filtered_img = cv2.filter2D(img, -1, gauss_matrix)
+        filtered_img = super_svertka(img, size, sigma)
+        gauss_filter = cv2.GaussianBlur(img, (size, size), sigma)
 
         cv2.imshow(f'Fileterd Image with size matrix = {size}, sigma = {sigma}', filtered_img)
+        cv2.imshow(f'Gaussian Blur with size matrix = {size}, sigma = {sigma}', gauss_filter)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
